354f4be8ffb7c0887ef923f6b6b8b1ed344eda44
rubensGo <lubisgolfetto@gmail.com>
2024-10-26 20:23:59 -0300

    testes

--- EXjava/TesteJ.java
+++ EXjava/TesteJ.java
@@ -1,25 +1,19 @@
 package EXjava;
 import java.util.Scanner;
+
 public class TesteJ {
   public static void main(String[] args) {
-    int opcoes = 0;
-    Scanner teclado = new Scanner(System.in);
-    while (opcoes != 3) { 
-      System.out.println("Qual opcao você quer? ");
-      System.out.println("[1] Calcular Imposto");
-      System.out.println("[2] Depositar salario");
-      System.out.println("[3] Sair");
-      opcoes = teclado.nextInt();
-      
-      if (opcoes == 1) {
-        System.out.println("Calculando imposto");
-      } else if (opcoes == 2) {
-        System.out.println("Depositar");
-      } else if (opcoes == 3) {
-        System.out.println("Saindo");
-      } else {
-        System.out.println("Opcao invalida");
-      }
+    System.out.println(" ");
+    System.out.println("Degite seu nome de usuario: ");
+    Scanner nome = new Scanner(System.in);
+    String username = nome.nextLine();
+    System.out.println("Confirme a sua senha: ");
+    Scanner senha = new Scanner(System.in);
+    String password = senha.nextLine();
+    if (username.equals("timeP") && password.equals("topP")) {
+      System.out.println("BEM VINDO "+ username);
+    }else {
+      System.out.println("ACESSO NEGADO");
     }
   }
 }
