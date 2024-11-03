8d10843394bc08cae839663c76478cde72bf4ea4
rubensGo <lubisgolfetto@gmail.com>
2024-11-03 11:42:23 -0300

    criaçao de gmail e senhas para os alunos

--- JavaTeste/Escolar.java
+++ JavaTeste/Escolar.java
@@ -1,45 +1,70 @@
 import java.util.Scanner;
-
+import java.time.LocalDate;
 public class Escolar {
   /*Cria um programa que armazena os nomes dos (5 alunos) de uma turma
     * mas na segunda vem todo mundo (8 alunos)
    * e imprime esses nomes
    */
+
+  /*---Atualização do sistema--- 
+   * Cria um programa que cria um gmail para cada aluno e uma senha para cada aluno
+   * Ex.: Nome: Pedro da Silva; Email: pedro-silva@gmail.com; Senha: pedro<aqui coloque o ano atual>
+   * Deixe o dia da semana automatico sem presizar escrever o dia da semana
+  */
   public static void main(String[] args) {
     System.out.println("");
-    System.out.println("Qual o dia da semana?");
-    Scanner diaDaSemana = new Scanner(System.in);
-    String day = diaDaSemana.nextLine();
+    String day2 = LocalDate.now().getDayOfWeek().toString();
+    int ano = LocalDate.now().getYear();
 
-    if (day.equalsIgnoreCase("segunda")) {
+    if (day2.equalsIgnoreCase("Monday")) {
       String[] nomeAlunos = new String[9];
-      Scanner aluno = new Scanner(System.in);
+      String[] sobrenome = new String[9];
 
-      
       for (int i = 0; i < 8; i++) {
         System.out.println("Nome do Aluno "+ (i+ 1));
+        Scanner aluno = new Scanner(System.in);
         nomeAlunos[i] = aluno.nextLine();
-      }
-      System.out.println("Nome dos Alunos");
-      for (int i = 0; i < 8; i++) {
-        System.out.println(nomeAlunos[i]);
-      }
+        
+        System.out.println("Sobrenome do Aluno "+ (i+ 1));
+        Scanner sobrenomeDoAluno = new Scanner(System.in);
+        sobrenome[i] = sobrenomeDoAluno.nextLine();
 
+        String[] gmail = new String[9];
+        String[] senha = new String[9];
+        gmail[i] = nomeAlunos[i]+"-"+sobrenome[i] + "@gmail.com";
+        senha[i] = nomeAlunos[i]+ ano;
+
+        System.out.println("Seu gmail "+ nomeAlunos[i] + " e este " + gmail[i]);
+        System.out.println("e sua senha eh " + senha[i]);
+        if (i >=8) {
+          System.out.println("Todas as conta dos alunos foram criadas");
+        }
+      }
     }else {
       String[] nomeAlunos = new String[6];
-      Scanner aluno = new Scanner(System.in);
+      
+      String[] gmail = new String[6];
+      String[] senha = new String[6];
+      String[] sobrenome = new String[6];
 
-      for (int i = 0; i < 5; i++) {
-        System.out.println("Nome do Aluno "+ (i+ 1));
-        nomeAlunos[i] = aluno.nextLine();
+          for (int i = 0; i < 5; i++) {
+            System.out.println("Nome do Aluno "+ (i+ 1));
+            Scanner aluno = new Scanner(System.in);
+            nomeAlunos[i] = aluno.nextLine();
+
+            System.out.println("Sobrenome do Aluno "+ (i+ 1));
+            Scanner sobrenomeDoAluno = new Scanner(System.in);
+            sobrenome[i] = sobrenomeDoAluno.nextLine();
+
+            gmail[i] = nomeAlunos[i]+"-"+sobrenome[i] + "@gmail.com";
+            senha[i] = nomeAlunos[i] + ano;
+            
+            System.out.println(nomeAlunos[i]+ " Seu gmail e este " + gmail[i]);
+            System.out.println(" e sua senha e " + senha[i]);
+            if (i == 5) {
+              System.out.println("Todas as conta dos alunos foram criadas");
+              }
       }
-      System.out.println("Nome dos Alunos");
-      System.out.println("Os nome dos alunos sao:");
-      for (int i = 0; i < 5; i++) {
-        System.out.println(nomeAlunos[i]);
-      }
-      aluno.close();
-    diaDaSemana.close();
     }
   }
 }
--- JavaTeste/TesteJ.java
+++ JavaTeste/TesteJ.java
@@ -2,7 +2,8 @@
 
 public class TesteJ {
     public static void main(String[] args) {
+
         System.out.println(" ");
-        
+
     }
 }
