746a8cdc8e236470ed5f6701a2a19cefc492163b
rubensGo <lubisgolfetto@gmail.com>
2024-11-21 09:59:02 -0300

    x

--- testeJava.java
+++ testeJava.java
@@ -2,13 +2,9 @@
 import java.util.Scanner;
 
 public class testeJava {
-    public void calcularSoma() {
-        double soma1 = 125;
-        double soma2 = 515;
-        double valor = soma1 + soma2;
-    }
+
     public static void main(String[] args) {
-        testeJava tj = new testeJava();
-        tj.calcularSoma();
+        
     }
 }
+
