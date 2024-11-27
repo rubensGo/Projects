f22dadcc17b2f631ff15d8eed13738d6ac048da6
rubensGo <lubisgolfetto@gmail.com>
2024-11-27 16:14:52 -0300

    v

--- TestesC#/obj/Debug/net9.0/TestesC#.AssemblyInfo.cs
+++ TestesC#/obj/Debug/net9.0/TestesC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestesC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e513cc03ff26048816ac9263370c0d7641abaad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0fc28075612f2650e158bc7aa84e37afac23fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestesC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestesC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
