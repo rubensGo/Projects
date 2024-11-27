bcb3a4569b857185e0b50d321733709606387794
rubensGo <lubisgolfetto@gmail.com>
2024-11-27 11:28:24 -0300

    mn

--- Javinha/teste.java
+++ Javinha/teste.java
@@ -3,11 +3,6 @@
 public class teste {
 
     public static void main(String[] args) {
-        System.out.println(" ");
-        System.out.println("Hello word!");
 
-        if (11 >10) {
-            System.out.println("9 maior que 10");
-        }
     }
 }
