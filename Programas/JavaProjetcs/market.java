e77b88ae285f8755f14774de12b7c0fe29b2d2cf
rubensGo <lubisgolfetto@gmail.com>
2024-12-07 22:24:58 -0300

    adf

--- Programas/JavaProjetcs/market.java
+++ Programas/JavaProjetcs/market.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class market {
 
     public static void main(String[] args) {
-        
+        // Opções do carrinho de compras
         String[] carrinho = new String[7];
         int prateleiraInt = 1;
 
@@ -17,7 +17,7 @@ public class market {
         double[] precos3 = {10.00, 9.00, 10.00, 11.00, 17.00, 21.00}; 
 
         while (true) {
-            System.out.println("""
+            String menu = JOptionPane.showInputDialog("""
                 1. adicionar produto no carrinho
                 2. remover produto do carrinho
                 3. trocar de prateleira
@@ -25,150 +25,88 @@ public class market {
                 5. ver todos os produtos do carrinho
                 6. finalizar compras
             """);
-            System.out.print("Escolha uma opcao: ");
-            Scanner opçoes = new Scanner(System.in);
-            int opcao = opçoes.nextInt();
+            int opcao = Integer.parseInt(menu);
 
             
             switch (opcao) {
                 case 1: //! opção de adicionar produto
-                    
-                    if (prateleiraInt == 1) {
-                        System.out.println("Produtos da prateleira 1#: ");
-                        System.out.println("""
-                        1.Coca-Cola
-                        2.Fanta
-                        3.Sprite
-                        4.Guarana
-                        5.Pepsi
-                        6.Cerveja""");
+                String[] produtos;
+                if (prateleiraInt == 1) {
+                    produtos = produtoDisp;
+                } else if (prateleiraInt == 2) {
+                    produtos = produtosDisp2;
+                } else {
+                    produtos = produtosDisp3;
+                }
 
-                        System.out.print("Digite o nome do produto: ");
-                        Scanner produtos = new Scanner(System.in);
-                        String produto = produtos.nextLine();
-                    
-                        if (produto.equalsIgnoreCase(produtoDisp[0]) || produto.equalsIgnoreCase(produtoDisp[1]) || produto.equalsIgnoreCase(produtoDisp[2]) || produto.equalsIgnoreCase(produtoDisp[3]) || produto.equalsIgnoreCase(produtoDisp[4]) || produto.equalsIgnoreCase(produtoDisp[5])) {
-                            
-                            System.out.print("Degite a posicao do carrinho (1-6): ");
-                            Scanner lugar = new Scanner(System.in);
-                            int lugarInt = lugar.nextInt();
-                            carrinho[lugarInt] = produto;
+                // Cria a janela de seleção de produtos
+                String produtoSelecionado = (String) JOptionPane.showInputDialog(
+                    null,
+                    "Escolha um produto da prateleira " + prateleiraInt + ":",
+                    "Prateleira " + prateleiraInt,
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    produtos,
+                    produtos[0]
+                );
 
-                            if (carrinho[lugarInt] == null) {
-                                System.out.println("Produto adicionado ao carrinho");
-                            }else{
-                                System.out.println(" ");
-                            }
-                        }else {
-                            System.out.println("Produto indisponivel");
-                        }
-                    }else if (prateleiraInt == 2) {
-                        System.out.println("Produtos da prateleira 2#: ");
-                        System.out.println("""
-                        1.Macarrao
-                        2.Chocolate
-                        3.Agua
-                        4.Massa
-                        5.Arroz
-                        6.Feijao""");
-                        System.out.print("Digite o nome do produto: ");
-                        Scanner produtos = new Scanner(System.in);
-                        String produto = produtos.nextLine();
-                    
-                        if (produto.equalsIgnoreCase(produtosDisp2[0]) || produto.equalsIgnoreCase(produtosDisp2[1]) || produto.equalsIgnoreCase(produtosDisp2[2]) || produto.equalsIgnoreCase(produtosDisp2[3]) || produto.equalsIgnoreCase(produtosDisp2[4]) || produto.equalsIgnoreCase(produtosDisp2[5])) {
-                            
-                            System.out.print("Degite a posicao do carrinho (1-6): ");
-                            Scanner lugar = new Scanner(System.in);
-                            int lugarInt = lugar.nextInt();
-                            carrinho[lugarInt] = produto;
+                if (produtoSelecionado != null) {
+                    String posicao = JOptionPane.showInputDialog("Digite a posição no carrinho (1-6):");
+                    int posicaoInt = Integer.parseInt(posicao);
 
-                            if (carrinho[lugarInt] == null) {
-                                System.out.println("Produto adicionado ao carrinho");
-                            }else{
-                                System.out.println("Posicao ocupada");
-                            }
-                        }else {
-                            System.out.println("Produto indisponivel");
-                        }
-                    }else {
-                        System.out.println("Produtos da prateleira 3#: ");
-                        System.out.println("""
-                        1.Molho-de-Tomate
-                        2.Vinagre
-                        3.Azeite
-                        4.Leite
-                        5.Ovo
-                        6.Farrofa""");
-                        System.out.print("Digite o nome do produto: ");
-                        Scanner produtos = new Scanner(System.in);
-                        String produto = produtos.nextLine();
-                    
-                        if (produto.equalsIgnoreCase(produtosDisp3[0]) || produto.equalsIgnoreCase(produtosDisp3[1]) || produto.equalsIgnoreCase(produtosDisp3[2]) || produto.equalsIgnoreCase(produtosDisp3[3]) || produto.equalsIgnoreCase(produtosDisp3[4]) || produto.equalsIgnoreCase(produtosDisp3[5])) {
-                            
-                            System.out.print("Degite a posicao do carrinho (1-6): ");
-                            Scanner lugar = new Scanner(System.in);
-                            int lugarInt = lugar.nextInt();
-                            carrinho[lugarInt] = produto;
-
-                            if (carrinho[lugarInt] == null) {
-                                System.out.println("Produto adicionado ao carrinho");
-
-                            }else{
-                                System.out.println("Posicao ocupada");
-                            }
-
-                        }else {
-                            System.out.println("Produto indisponivel");
-                        }
+                    if (posicaoInt >= 0 && posicaoInt < carrinho.length && carrinho[posicaoInt] == null) {
+                        carrinho[posicaoInt] = produtoSelecionado;
+                        JOptionPane.showMessageDialog(null, "Produto adicionado ao carrinho!");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Posição inválida ou já ocupada!");
                     }
-                    break;
+                }
+                break;
 
                 case 2: //! opção de remover produto
-                    System.out.print("Digite a posicao do carrinho para remover (1-6): ");
-                    Scanner posiçao = new Scanner(System.in);
-                    int posi = posiçao.nextInt();
+                    String posicao = JOptionPane.showInputDialog("Digite a posicao do carrinho para remover (1-6): ");
+                    int posi = Integer.parseInt(posicao);
                     if (posi > 0 && posi < 7) {
                         if (carrinho[posi] != null) {
                             carrinho[posi] = null;
-                            System.out.println("Produto removido do carrinho");
+                            JOptionPane.showMessageDialog(null, ("Produto removido do carrinho"));
 
                     }else{
-                        System.out.println("Posicao vazia");
+                        JOptionPane.showMessageDialog(null, "Posicao vazia");
                     }
 
                 }else {
-                    System.out.println("Posicao invalida");
+                    JOptionPane.showMessageDialog(null, ("Posicao invalida"));
                 }
                     break;
 
                 case 3: //! opção de trocar de prateleira
-                    System.out.println("Qual prateleira tu quer ir? (1, 2 ou 3)");
-                    Scanner prateleira = new Scanner(System.in);
-                    prateleiraInt = prateleira.nextInt();
+                    String prateleiraEscolha = JOptionPane.showInputDialog("Qual prateleira tu quer ir? (1, 2 ou 3)");
+                    prateleiraInt = Integer.parseInt(prateleiraEscolha);
                     break;
 
                 case 4: //! opção de voltar para a prateleira
                 if (prateleiraInt == 1) {
-                    System.out.println("Qual prateleira tu quer voltar? (2 ou 3)");
-                    Scanner voltaPrateleira = new Scanner(System.in);
-                    prateleiraInt = voltaPrateleira.nextInt();
+                    String prateleiraVolta = JOptionPane.showInputDialog("Qual prateleira tu quer voltar? (2 ou 3)");
+                    
+                    prateleiraInt = Integer.parseInt(prateleiraVolta);;
                     if (prateleiraInt == 1) {
                         prateleiraInt = 2;
                     }
 
                 }else if (prateleiraInt == 2) {
-                    System.out.println("Qual prateleira tu quer voltar? (1 ou 3)");
-                    Scanner voltaPrateleira = new Scanner(System.in);
-                    prateleiraInt = voltaPrateleira.nextInt();
+                    String prateleiraVolta = JOptionPane.showInputDialog("Qual prateleira tu quer voltar? (1 ou 3)");
+                    
+                    prateleiraInt = Integer.parseInt(prateleiraVolta);
                     if (prateleiraInt == 2) {
                         prateleiraInt = 3;
                     }
 
                 }else {
-                    System.out.println("Qual prateleira tu quer voltar? (1 ou 2)");
+                    String prateleiraVolta = JOptionPane.showInputDialog("Qual prateleira tu quer voltar? (1 ou 2)");
 
-                    Scanner voltaPrateleira = new Scanner(System.in);
-                    prateleiraInt = voltaPrateleira.nextInt();
+                    
+                    prateleiraInt = Integer.parseInt(prateleiraVolta);
                     if (prateleiraInt == 3) {
                         prateleiraInt = 2;
                     }
@@ -176,52 +114,67 @@ public class market {
                     break;
 
                 case 5: //! opção de ver todos os produtos do carrinho
-                    System.out.println("Produtos no carrinho: ");
+                    JOptionPane.showMessageDialog(null, ("Produtos no carrinho: "));
                     for (int i = 0; i < carrinho.length; i++) {
                         if (carrinho[i] != null) {
-                            System.out.println((i) + ". " + carrinho[i]);
+                            JOptionPane.showMessageDialog(null, ((i) + ". " + carrinho[i]));
                         }
                     }
                     break;
+
                 case 6: //! opção de finalizar compras
-                double total = 0;
-                System.out.println("Finalizando compras...");
-
-                // Exibe os produtos no carrinho e calcula o total
-                for (int i = 0; i < carrinho.length; i++) {
-                    if (carrinho[i] != null) {
-                        String produtoNoCarrinho = carrinho[i];
-                        // Encontrar o preço do produto
-                        for (int j = 0; j < produtoDisp.length; j++) {
-                            if (produtoNoCarrinho.equalsIgnoreCase(produtoDisp[j])) {
-                                total += precos[j];
-                                System.out.println(produtoNoCarrinho + " - R$ " + precos[j]);
-                                System.exit(0);
-                                break;
+                    double total = 0;
+                    StringBuilder resumo = new StringBuilder("Finalizando compras...\n\n");
+                
+                    // Exibe os produtos no carrinho e calcula o total
+                    for (int i = 0; i < carrinho.length; i++) {
+                        if (carrinho[i] != null) {
+                            String produtoNoCarrinho = carrinho[i];
+                            boolean produtoEncontrado = false;
+                
+                            // Encontrar o preço do produto na prateleira 1
+                            for (int j = 0; j < produtoDisp.length; j++) {
+                                if (produtoNoCarrinho.equalsIgnoreCase(produtoDisp[j])) {
+                                    total += precos[j];
+                                    resumo.append(produtoNoCarrinho).append(" - R$ ").append(precos[j]).append("\n");
+                                    produtoEncontrado = true;
+                                    break;
+                                }
                             }
-                        }
-
-                        for (int j = 0; j < produtosDisp2.length; j++) {
-                            if (produtoNoCarrinho.equalsIgnoreCase(produtosDisp2[j])) {
-                                total += precos2[j];
-                                System.out.println(produtoNoCarrinho + " - R$ " + precos2[j]);
-                                System.exit(0);
-                                break;
+                
+                            // Encontrar o preço do produto na prateleira 2
+                            if (!produtoEncontrado) {
+                                for (int j = 0; j < produtosDisp2.length; j++) {
+                                    if (produtoNoCarrinho.equalsIgnoreCase(produtosDisp2[j])) {
+                                        total += precos2[j];
+                                        resumo.append(produtoNoCarrinho).append(" - R$ ").append(precos2[j]).append("\n");
+                                        produtoEncontrado = true;
+                                        break;
+                                    }
+                                }
                             }
-                        }
-
-                        for (int j = 0; j < produtosDisp3.length; j++) {
-                            if (produtoNoCarrinho.equalsIgnoreCase(produtosDisp3[j])) {
-                                total += precos3[j];
-                                System.out.println(produtoNoCarrinho + " - R$ " + precos3[j]);
-                                System.exit(0);
-                                break;
+                
+                            // Encontrar o preço do produto na prateleira 3
+                            if (!produtoEncontrado) {
+                                for (int j = 0; j < produtosDisp3.length; j++) {
+                                    if (produtoNoCarrinho.equalsIgnoreCase(produtosDisp3[j])) {
+                                        total += precos3[j];
+                                        resumo.append(produtoNoCarrinho).append(" - R$ ").append(precos3[j]).append("\n");
+                                        break;
+                                    }
+                                }
                             }
                         }
                     }
+                
+                    // Exibe o total
+                    resumo.append("\nTotal: R$ ").append(String.format("%.2f", total));
+                    JOptionPane.showMessageDialog(null, resumo.toString());
                     
-                }
+                    // Sair do programa
+                    System.exit(0);
+                    break;
             }
         }
-        }
     }
+}
