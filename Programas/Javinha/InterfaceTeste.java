f8bf55956c6ac1eab73c8538070a62dd85ad39cd
rubensGo <lubisgolfetto@gmail.com>
2024-12-21 12:44:30 -0300

    a

--- Programas/Javinha/InterfaceTeste.java
+++ Programas/Javinha/InterfaceTeste.java
@@ -1,14 +1,4 @@
 
 public interface InterfaceTeste {
 
-    public void abrir();
-
-    public void fechar();
-
-    public void folhear();
-
-    public void avancarPag();
-
-    public void voltarPag();
-
 }
--- Programas/Javinha/teste.java
+++ Programas/Javinha/teste.java
@@ -1,13 +1,9 @@
-// !classe Main!
-
+// !classe Main! 
 //import javax.swing.JOptionPane;
 
 public class teste {
 
     public static void main(String[] args) {
-        TesteObj obj = new TesteObj("Javinha", "Rubens Golfetto", 3);
-        obj.abrir();
 
-        obj.fechar();
     }
 }
